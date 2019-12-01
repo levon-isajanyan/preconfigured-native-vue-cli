784e2bc7d447cae1e045f0a8d5c84200091e8fcd
Levon Isajanyan <levonisadjanyan@gmail.com>
2019-12-01 21:50:49 +0400

    Some rules changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
       // override/add rules settings here, such as:
       // 'vue/no-unused-vars': 'error'
       'vue/name-property-casing': ['error', 'PascalCase'],
-      'vue/no-spaces-around-equal-signs-in-attribute': ['error'],
       'vue/no-boolean-default': ['error', 'default-false'],
       'vue/v-on-function-call': ['error', 'never']
     }
